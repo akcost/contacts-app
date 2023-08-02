878517d4f99376684c791a6a01a7db4e229af6c5
akcost <akselcosta@gmail.com>
2023-08-02 15:45:54 +0300

    Added saving a contact, getting contact by id, deleting contact by id to ContactController.

--- src/main/java/app/ContactController.java
+++ src/main/java/app/ContactController.java
@@ -1,7 +1,9 @@
 package app;
 
 import dao.ContactDao;
+import jakarta.validation.Valid;
 import model.Contact;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,5 +22,22 @@ public class ContactController {
     @GetMapping("/contacts")
     public List<Contact> getContacts() {
         return dao.findAll();
+    }
+
+    @GetMapping("/contacts/{id}")
+    public Contact getContactById(@PathVariable Long id) {
+        return dao.findContactById(id);
+    }
+
+    @PostMapping("/contacts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Contact save(@RequestBody @Valid Contact contact) {
+        return dao.save(contact);
+    }
+
+    @DeleteMapping("/contacts/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteContactById(@PathVariable Long id) {
+        dao.deleteContactById(id);
     }
 }
