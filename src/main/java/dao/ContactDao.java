cdba69b411a1a3ff47bb2e68115ad2f436f260a1
akcost <akselcosta@gmail.com>
2023-08-02 15:43:46 +0300

    Added find by id, delete by id and saving a contact.

--- src/main/java/dao/ContactDao.java
+++ src/main/java/dao/ContactDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import model.Contact;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +14,31 @@ public class ContactDao {
     @PersistenceContext
     private EntityManager em;
 
+    @Transactional
+    public Contact save(Contact contact) {
+        if (contact.getId() == null) {
+            em.persist(contact);
+        } else {
+            em.merge(contact);
+        }
+
+        return contact;
+    }
+
     public List<Contact> findAll() {
         return em.createQuery("select p from Contact p").getResultList();
+    }
+
+    public Contact findContactById(Long id) {
+        return em.find(Contact.class, id);
+    }
+
+    @Transactional
+    public void deleteContactById(Long id) {
+        Contact contact = em.find(Contact.class, id);
+
+        if (contact != null) {
+            em.remove(contact);
+        }
     }
 }
